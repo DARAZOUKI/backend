1354dd171a141ab5ccfcf05a7a44e66d7edba18a
DARAZOUKI <Aussama@chalmers.se>
2025-03-23 22:06:25 +0100

    edit

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.enableCors({
-    origin:['https://react-bookreviewapp.netlify.app/'], // Allow frontend origin
+    origin:['https://react-bookreviewapp.netlify.app'], // Allow frontend origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
   });
